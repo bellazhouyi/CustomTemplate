113ec8faccce6fac5330438215c49981b3ab523e
ZZH <zhangzhiheng@cqt.cc>
2017-01-24 09:27:53 +0800

    pod install Masory

---
+++ BellaTemplate/BellaTemplate/Custom/Category/NSString/NSString+Attributed.h
@@ -0,0 +1,15 @@
+//
+//  NSString+Attributed.h
+//  BellaTemplate
+//
+//  Created by ANine on 22/01/2017.
+//  Copyright © 2017 bella. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Attributed)
+
+
+
+@end
---
+++ BellaTemplate/BellaTemplate/Custom/Category/NSString/NSString+Attributed.m
@@ -0,0 +1,18 @@
+//
+//  NSString+Attributed.m
+//  BellaTemplate
+//
+//  Created by ANine on 22/01/2017.
+//  Copyright © 2017 bella. All rights reserved.
+//
+
+#import "NSString+Attributed.h"
+@import UIKit;
+
+@implementation NSString (Attributed)
+
+- (void)addAtrributedColor:(UIColor *)color range:(NSRange)range {
+    
+}
+
+@end
